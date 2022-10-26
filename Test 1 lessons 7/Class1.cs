ea044a757377b695dc73d16782135e6eadbd20af
JonEgor <114680645+JonEgor@users.noreply.github.com>
2022-10-26 22:55:33 +0300

    Добавьте файлы проекта.

---
+++ Test 1 lessons 7/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Debt;
+
+Debt1 mortgage = new Debt1(120000.0, 1.01);
+    mortgage.PrintBalance();
+
+    mortgage.WaitOneYear();
+    mortgage.PrintBalance();
+
+    //Wait 20 years
+    int years = 0;
+    while (years < 20)
+    {
+        mortgage.WaitOneYear();
+        years = years + 1;
+    }
+    mortgage.PrintBalance();
---
+++ Test 1 lessons 7/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+
+namespace Debt
+{
+     internal class Debt1
+    {
+        private double _initialBalance;
+            private double _interestRate;
+
+
+        public Debt1(double balance, double interestRate)
+        {
+            _initialBalance = balance;
+            _interestRate = interestRate; 
+        }
+        public void PrintBalance()
+        {
+            Console.WriteLine($"Your dept is: {_initialBalance}");
+
+        }
+        public void WaitOneYear()
+        {
+            _initialBalance *= _interestRate;
+        }
+        
+
+    }
+}
